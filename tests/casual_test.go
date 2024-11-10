cf39322ee7ed7519d5317ad7b94fede60f671af5
yifeitai <neversaynever1022@gmail.com>
2024-11-10 16:01:02 +0800

    add lexer initially

---
+++ tests/casual_test.go
@@ -0,0 +1,11 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+
+	fmt.Println("hwhw")
+}
--- token/token.go
+++ token/token.go
@@ -1,7 +1,6 @@
 package token
 
 type TokenType string
-
 type Token struct {
 	Type    TokenType
 	Literal string
@@ -12,4 +11,19 @@ const (
 	EOF     = "EOF"
 
 	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
 )
